71a3363031ca737b35066f97529b52c6965975b4
phischu <pschuster@uni-koblenz.de>
2014-05-19 12:22:38 +0200

    hello neo4j

--- src/main/java/declarationimport/Main.java
+++ src/main/java/declarationimport/Main.java
@@ -1,8 +1,17 @@
 package declarationimport;
 
+import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.graphdb.factory.GraphDatabaseFactory;
+
 public class Main {
 	
+	private static final String DB_PATH = "data";
+
 	public static void main(String[] args){
+		
+		GraphDatabaseService graphDb = new GraphDatabaseFactory().newEmbeddedDatabase( DB_PATH );
+		graphDb.shutdown();
+		
 		System.out.println("hallo");
 	}
 
