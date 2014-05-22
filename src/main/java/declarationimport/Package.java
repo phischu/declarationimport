f5f1685f8d5f076942158aa99f67b22359347d2b
phischu <pschuster@uni-koblenz.de>
2014-05-22 17:01:21 +0200

    package and dependency datatypes

---
+++ src/main/java/declarationimport/Package.java
@@ -0,0 +1,11 @@
+package declarationimport;
+
+import java.util.Collection;
+
+public class Package {
+	
+	public String packagename;
+	public String packageversion;
+	public Collection<Dependency> dependencies;
+
+}
